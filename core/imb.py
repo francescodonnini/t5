ee79bb03ad52349be0a34a01732f22fd9df13b27
Francesco Donnini <donnini.francesco00@gmail.com>
2025-02-24 21:48:02 +0100

    fixed class_weights

--- core/imb.py
+++ core/imb.py
@@ -3,13 +3,14 @@ import numpy.typing as npt
 
 
 def class_weights(y: npt.NDArray, beta: float=0.9999) -> dict[int, float]:
-    def expected_volume(n: int) -> float:
-        return (1 - beta ** n)/(1 - beta)
-
     pos = np.count_nonzero(y)
     if pos == 0:
         pos = 1
     neg = len(y) - pos
     if neg == 0:
         neg = 1
-    return { 0: 1/expected_volume(pos), 1: 1/expected_volume(neg) }
+    samples_per_class = np.maximum([neg, pos], 1)
+    effective_num = 1. - np.power(beta, samples_per_class)
+    weights = (1. - beta) / np.array(effective_num)
+    weights /= np.sum(weights) * 2
+    return { 0: float(weights[0]), 1: float(weights[1]) }
